fa0ab4b618553239f77daae52e009cde3ee29d50
Ulugbek <ulugbek.abdimanabov@3i.ai>
2021-06-27 22:13:01 +0500

    Now camera can be used

--- video_face_detector/main.cpp
+++ video_face_detector/main.cpp
@@ -7,29 +7,42 @@
 using namespace cv;
 using namespace std;
 
+const bool using_camera = false;
+
 void main()
 {
 
-	string filepath = "Resources/face_rotating.mp4";
-	
+	const string filepath = "Resources/face_rotating.mp4";
+
+#ifdef using_camera
+
+	const int camera_index = 0; 
+
+	VideoCapture video(camera_index);
+
+#else
+
 	VideoCapture video(filepath);
+
+#endif
+
 
 	CascadeClassifier faceCascade;
 	faceCascade.load("Resources/haarcascade_frontalface_default.xml");
-	if (faceCascade.empty()) { cout << "XML file not loaded" << endl; }
+	if(faceCascade.empty()) { cout << "XML file not loaded" << endl; }
 
 	Mat img;
 
-	while (true)
+	while(video.isOpened())
 	{
-		for(int skip = 0; skip < 10 && video.isOpened(); skip++)
+		for(int skip = 0; skip < 5 && video.isOpened(); skip++)
 			video.read(img);
 
 		vector<Rect> faces;
 		faceCascade.detectMultiScale(img, faces, 1.1, 10);
 
-		for (size_t i = 0; i < faces.size(); i++)
-			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+		for(size_t i = 0; i < faces.size(); i++)
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(66, 255, 66), 3);
 
 		imshow("Image", img);
 		waitKey(1);
