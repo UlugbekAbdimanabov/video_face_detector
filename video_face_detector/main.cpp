8ffc780a74aff86e5d3e4fa95cae8303e02b9048
Ulugbek <ulugbek.abdimanabov@3i.ai>
2021-06-28 12:48:38 +0500

    profile face recognition, don't show to speed it up

--- video_face_detector/main.cpp
+++ video_face_detector/main.cpp
@@ -9,42 +9,162 @@ using namespace std;
 
 const bool using_camera = false;
 
-void main()
+int main()
 {
 
-	const string filepath = "Resources/face_rotating.mp4";
+       const string filepath = "Resources/face_rotating_2.mp4";
 
 #ifdef using_camera
 
-	const int camera_index = 0; 
+       const int camera_index = 0;
 
-	VideoCapture video(camera_index);
+       VideoCapture video(camera_index);
 
 #else
 
-	VideoCapture video(filepath);
+       VideoCapture video(filepath);
 
 #endif
 
+       bool front_face = false, profile_face = false;
 
-	CascadeClassifier faceCascade;
-	faceCascade.load("Resources/haarcascade_frontalface_default.xml");
-	if(faceCascade.empty()) { cout << "XML file not loaded" << endl; }
+       CascadeClassifier faceCascade;
+       Mat img;
+       ////////////////////// Check the face from front side ////////////////////////////////
 
-	Mat img;
+       faceCascade.load("Resources/haarcascade_frontalface_default.xml");
+       if(faceCascade.empty())
+              cout << "Frontalface XML file not loaded" << endl;
 
-	while(video.isOpened())
-	{
-		for(int skip = 0; skip < 5 && video.isOpened(); skip++)
-			video.read(img);
+       while(video.isOpened())
+       {
+              for(int skip = 0; skip < 2 && video.isOpened(); skip++)
+                     video.read(img);
 
-		vector<Rect> faces;
-		faceCascade.detectMultiScale(img, faces, 1.1, 10);
+              if(img.empty())
+                     break;
 
-		for(size_t i = 0; i < faces.size(); i++)
-			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(66, 255, 66), 3);
+              vector<Rect> faces;
+              faceCascade.detectMultiScale(img, faces, 1.1, 3, 0, Size(250, 250));
 
-		imshow("Image", img);
-		waitKey(1);
-	}
+              for(size_t i = 0; i < faces.size(); i++)
+              {
+                     for(int j = 0; j < 1e7; j++);
+                     rectangle(img, faces[i].tl(), faces[i].br(), Scalar(66, 255, 66), 3);
+              }
+
+              front_face |= !faces.empty();
+
+              imshow("Image", img);
+
+              waitKey(1);
+       }
+
+       ////////////////////// Check the face from front side FINISHED ////////////////////////////////
+
+
+
+       ////////////////////// Check the face profile ////////////////////////////////
+
+       // video has been finished so reopen it
+       video.open(filepath);
+       //faceCascade.load("Resources/haarcascade_profileface.xml");
+       CascadeClassifier faceCascade2;
+       faceCascade2.load("Resources/right_profile_face.xml");
+       if(faceCascade2.empty())
+              cout << "Profileface XML file not loaded" << endl;
+
+
+       while(video.isOpened())
+       {
+              for(int skip = 0; skip < 2 && video.isOpened(); skip++)
+                     video.read(img);
+
+              if(img.empty())
+                     break;
+              
+              // original version 
+
+              vector<Rect> faces;
+              faceCascade2.detectMultiScale(img, faces, 1.1, 3, 0, Size(250, 250));
+
+              for(size_t i = 0; i < faces.size(); i++)
+              {
+                     waitKey(100);
+                     rectangle(img, faces[i].tl(), faces[i].br(), Scalar(66, 255, 66), 3);
+              }
+
+              profile_face |= !faces.empty();
+
+              imshow("Image", img);
+
+              waitKey(1);
+#if 0
+              // do it now with the flipped one
+
+              flip(img, img, 1);
+              faces.clear();
+
+              faceCascade2.detectMultiScale(img, faces, 1.1, 3, 0, Size(250, 250));
+
+              for(size_t i = 0; i < faces.size(); i++)
+              {
+                     waitKey(100);
+                     rectangle(img, faces[i].tl(), faces[i].br(), Scalar(66, 255, 66), 3);
+              }
+
+              profile_face |= !faces.empty();
+
+              imshow("Image", img);
+
+              waitKey(1);
+#endif
+       }
+
+
+       // flipped version
+       // video has been finished so reopen it
+       video.open(filepath);
+       
+       while(video.isOpened())
+       {
+              for(int skip = 0; skip < 2 && video.isOpened(); skip++)
+                     video.read(img);
+
+              if(img.empty())
+                     break;
+
+              // flipped version 
+              flip(img, img, 1);
+              vector<Rect> faces;
+              faceCascade2.detectMultiScale(img, faces, 1.1, 3, 0, Size(250, 250));
+
+              for(size_t i = 0; i < faces.size(); i++)
+              {
+                     waitKey(100);
+                     rectangle(img, faces[i].tl(), faces[i].br(), Scalar(66, 255, 66), 3);
+              }
+
+              profile_face |= !faces.empty();
+
+              imshow("Image", img);
+
+              waitKey(1);
+       }
+
+       ////////////////////// Check the face from profile FINISHED ////////////////////////////////
+
+
+       if(front_face && profile_face)
+              cout << "Face was found and it was rotated!" << endl;
+       else
+       {
+              if(!front_face && profile_face)
+                     cout << "Front face was found but we couldn't detect it from profile view!" << endl;
+              else if(front_face && profile_face)
+                     cout << "Front face was found but we couldn't detect it from profile view!" << endl;
+              else
+                     cout << "No faces were detected!" << endl;
+       }
+       return 0;
 }
